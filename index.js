3a011cafa19caae67c35bbb55cf554d43f79c751
Angela <chepkoechangela514@gmail.com>
2022-08-21 20:14:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-const titleCased = () => {
-  return tutorials
-}
+// Code your solution here
+const totalBatteries = batteryBatches.reduce(
+  (acc, battery) => (acc += battery),
+  0
+);
